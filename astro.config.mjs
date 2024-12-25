d4b36a6f5e9d5f00a07f7fbeece62d37ac231b1a
Heru Handika <herubiolog@gmail.com>
2024-12-24 18:32:39 -0600

    Add db.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hhandika.github.io",
   base: "mammaldiversity-org",
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
 });
