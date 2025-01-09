91f41bc99ae23011b123fca73e8375f36c7e2c3c
Heru Handika <herubiolog@gmail.com>
2025-01-09 13:28:26 -0600

    Improve synonym parsing.

--- db/mdd.ts
+++ db/mdd.ts
@@ -1,6 +1,7 @@
 // Parser for parsing the MDD json file
 import fs from "fs";
-import type { MddData, Synonym, Taxonomy } from "./model";
+import type { MddData, Synonym, SynonymName, Taxonomy } from "./model";
+import { createAuthorityCitation, createSynonymName } from "../src/scripts/taxon";
 
 function parseMDDJson(): MddData {
   const rawData = fs.readFileSync("./db/data/mdd.json", "utf8");
@@ -8,12 +9,12 @@ function parseMDDJson(): MddData {
   return jsonData;
 }
 
-export function getSpeciesData(): Taxonomy[] {
+function getSpeciesData(): Taxonomy[] {
   return parseMDDJson().data;
 }
 
 // Parse the MDD json file and return the taxonomy data
-export function getTaxonomyData(speciesId: number): Taxonomy {
+function getTaxonomyData(speciesId: number): Taxonomy {
   // Find the taxonomy data based on the speciesID
   const data = parseMDDJson().data;
   const taxonomy = data
@@ -22,9 +23,29 @@ export function getTaxonomyData(speciesId: number): Taxonomy {
   return taxonomy || ({} as Taxonomy);
 }
 
-export function getSynonymData(speciesId: number): Synonym[] {
+function filterSynonymData(synonym: Synonym[], speciesId: number): Synonym[] {
   // Filter the synonyms based on the speciesID
   return parseMDDJson().synonyms.filter(
     (synonym) => synonym.speciesId === speciesId
   );
 }
+
+function getSynonymName(synonym: Synonym[], speciesId: number): SynonymName[] {
+  const data = filterSynonymData(synonym, speciesId);
+  return data.map((synonym) => {
+    const name = createSynonymName(synonym.rootName, synonym.species);
+    const citation = createAuthorityCitation(
+      synonym.author,
+      synonym.year,
+      synonym.authorityParentheses === 1
+    );
+    return { name, citation };
+  });
+}
+
+
+function getAllSynonyms(): Synonym[] {
+  return parseMDDJson().synonyms;
+}
+
+export { getSpeciesData, getTaxonomyData, filterSynonymData, getAllSynonyms, getSynonymName };
--- src/scripts/taxon.ts
+++ src/scripts/taxon.ts
@@ -1,4 +1,17 @@
 // Check if the taxon data contains "|" and replace it with  middle dot "·"
-export function cleanTaxonData(taxonData: string): string {
+function cleanTaxonData(taxonData: string): string {
   return taxonData.includes("|") ? taxonData.split("|").join(" · ") : taxonData;
 }
+
+function createSynonymName(rootName: string, species: string, originalNameCombination?: string): string {
+  return originalNameCombination
+  ? originalNameCombination
+  : `${species} ${rootName}`
+}
+
+function createAuthorityCitation(authority: string, year: number, withParentheses: boolean): string {
+  let citation = `${authority}, ${year}`;
+  return withParentheses ? `(${citation})` : citation;
+}
+
+export { createSynonymName, cleanTaxonData, createAuthorityCitation };
