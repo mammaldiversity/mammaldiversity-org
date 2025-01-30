4a695f9491a1cb7597073903ec04ae55b97003cb
Heru Handika <herubiolog@gmail.com>
2025-01-30 16:39:55 -0600

    Add page searching modules.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import db from "@astrojs/db";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hhandika.github.io",
   base: "mammaldiversity-org",
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), pagefind()],
 });
