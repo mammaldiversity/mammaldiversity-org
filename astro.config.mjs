88eff9eda14bbc78cc82f10c095fadf3c4c09377
Heru Handika <herubiolog@gmail.com>
2025-02-13 23:48:08 -0600

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import db from "@astrojs/db";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hhandika.github.io",
   base: "mammaldiversity-org",
-  integrations: [tailwind(), db(), pagefind()],
+  integrations: [tailwind(), pagefind()],
 });
