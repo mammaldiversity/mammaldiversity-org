202231b52b3ceaf4c83926bc643a1d264b5d97c2
Heru Handika <herubiolog@gmail.com>
2025-02-15 16:14:02 -0600

    Handle potential distrubution.

--- src/scripts/map.ts
+++ src/scripts/map.ts
@@ -1,251 +1,27 @@
-import leaflet from "leaflet";
-
-const assetUrl =
-  "https://raw.githubusercontent.com/mammaldiversity/mammaldiversity.github.io/refs/heads/master/assets/countries/";
-
-export function drawCountriesOnMap(
-  countryDistribution: string,
-  elementId: string
-) {
-  let countries = splitCountryDistribution(countryDistribution);
-  let countryCodes = matchAllCountriesToCodeName(countries);
-
-  // initialize Leaflet
-  let map = leaflet.map(elementId);
-
-  // add the OpenStreetMap tiles
-  leaflet
-    .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-    })
-    .addTo(map);
-
-  let distribution = new leaflet.FeatureGroup();
-  countryCodes.forEach(function (countryCode) {
-    async function addGeoJson() {
-      const response = await fetch(assetUrl + countryCode + ".json");
-      const shape = await response.json();
-      if (shape) {
-        distribution.addLayer(leaflet.geoJSON(shape));
-        distribution.addTo(map);
-        if (distribution.getLayers().length > 0) {
-          map.fitBounds(distribution.getBounds());
-        }
-      }
-    }
-
-    addGeoJson();
-  });
+interface CountryDistribution {
+  known: string[];
+  potential: string[];
 }
 
-export function splitCountryDistribution(
-  countryDistribution: string
-): string[] {
-  return countryDistribution
-    .split("|")
-    .map((country) => country.trim().replace(/\?$/, ""));
+function splitCountryDistribution(countryDistribution: string): string[] {
+  return countryDistribution.split("|");
 }
 
-export function matchAllCountriesToCodeName(countries: string[]): string[] {
-  return countries.map((country) => countryCode[country]);
+function countryListToJson(countryList: string[]): string {
+  return JSON.stringify(countryList);
 }
 
-const countryCode: { [key: string]: string } = {
-  Afghanistan: "AF",
-  Albania: "AL",
-  Algeria: "DZ",
-  Andorra: "AD",
-  Angola: "AO",
-  "Antigua and Barbuda": "AG",
-  Argentina: "AR",
-  Armenia: "AM",
-  Australia: "AU",
-  Austria: "AT",
-  Azerbaijan: "AZ",
-  Bahamas: "BS",
-  Bahrain: "BH",
-  Bangladesh: "BD",
-  Barbados: "BB",
-  Belarus: "BY",
-  Belgium: "BE",
-  Belize: "BZ",
-  Benin: "BJ",
-  Bhutan: "BT",
-  Bolivia: "BO",
-  "Bosnia and Herzegovina": "BA",
-  Botswana: "BW",
-  Brazil: "BR",
-  Brunei: "BN",
-  Bulgaria: "BG",
-  "Burkina Faso": "BF",
-  Burundi: "BI",
-  "Cabo Verde": "CV",
-  Cambodia: "KH",
-  Cameroon: "CM",
-  Canada: "CA",
-  "Central African Republic": "CF",
-  Chad: "TD",
-  Chile: "CL",
-  China: "CN",
-  Colombia: "CO",
-  Comoros: "KM",
-  Congo: "CG",
-  "Costa Rica": "CR",
-  Croatia: "HR",
-  Cuba: "CU",
-  Cyprus: "CY",
-  "Czech Republic": "CZ",
-  "Democratic Republic of the Congo": "CD",
-  Denmark: "DK",
-  Djibouti: "DJ",
-  Dominica: "DM",
-  "Dominican Republic": "DO",
-  Ecuador: "EC",
-  Egypt: "EG",
-  "El Salvador": "SV",
-  "Equatorial Guinea": "GQ",
-  Eritrea: "ER",
-  Estonia: "EE",
-  Eswatini: "SZ",
-  Ethiopia: "ET",
-  Fiji: "FJ",
-  Finland: "FI",
-  France: "FR",
-  Gabon: "GA",
-  Gambia: "GM",
-  Georgia: "GE",
-  Germany: "DE",
-  Ghana: "GH",
-  Greece: "GR",
-  Grenada: "GD",
-  Guatemala: "GT",
-  Guinea: "GN",
-  "Guinea-Bissau": "GW",
-  Guyana: "GY",
-  Haiti: "HT",
-  Honduras: "HN",
-  Hungary: "HU",
-  Iceland: "IS",
-  India: "IN",
-  Indonesia: "ID",
-  Iran: "IR",
-  Iraq: "IQ",
-  Ireland: "IE",
-  Israel: "IL",
-  Italy: "IT",
-  "Ivory Coast": "CI",
-  Jamaica: "JM",
-  Japan: "JP",
-  Jordan: "JO",
-  Kazakhstan: "KZ",
-  Kenya: "KE",
-  Kiribati: "KI",
-  "North Korea": "KP",
-  "South Korea": "KR",
-  Kuwait: "KW",
-  Kyrgyzstan: "KG",
-  Laos: "LA",
-  Latvia: "LV",
-  Lebanon: "LB",
-  Lesotho: "LS",
-  Liberia: "LR",
-  Libya: "LY",
-  Liechtenstein: "LI",
-  Lithuania: "LT",
-  Luxembourg: "LU",
-  Madagascar: "MG",
-  Malawi: "MW",
-  Malaysia: "MY",
-  Maldives: "MV",
-  Mali: "ML",
-  Malta: "MT",
-  "Marshall Islands": "MH",
-  Mauritania: "MR",
-  Mauritius: "MU",
-  Mexico: "MX",
-  Micronesia: "FM",
-  Moldova: "MD",
-  Monaco: "MC",
-  Mongolia: "MN",
-  Montenegro: "ME",
-  Morocco: "MA",
-  Mozambique: "MZ",
-  Myanmar: "MM",
-  Namibia: "NA",
-  Nauru: "NR",
-  Nepal: "NP",
-  Netherlands: "NL",
-  "New Zealand": "NZ",
-  Nicaragua: "NI",
-  Niger: "NE",
-  Nigeria: "NG",
-  "North Macedonia": "MK",
-  Norway: "NO",
-  Oman: "OM",
-  Pakistan: "PK",
-  Palau: "PW",
-  Panama: "PA",
-  "Papua New Guinea": "PG",
-  Paraguay: "PY",
-  Peru: "PE",
-  Philippines: "PH",
-  Poland: "PL",
-  Portugal: "PT",
-  Qatar: "QA",
-  Romania: "RO",
-  Russia: "RU",
-  Rwanda: "RW",
-  "Saint Kitts and Nevis": "KN",
-  "Saint Lucia": "LC",
-  "Saint Vincent and the Grenadines": "VC",
-  Samoa: "WS",
-  "San Marino": "SM",
-  "Sao Tome and Principe": "ST",
-  "Saudi Arabia": "SA",
-  Senegal: "SN",
-  Serbia: "RS",
-  Seychelles: "SC",
-  "Sierra Leone": "SL",
-  Singapore: "SG",
-  Slovakia: "SK",
-  Slovenia: "SI",
-  "Solomon Islands": "SB",
-  Somalia: "SO",
-  "South Africa": "ZA",
-  "South Sudan": "SS",
-  Spain: "ES",
-  "Sri Lanka": "LK",
-  Sudan: "SD",
-  Suriname: "SR",
-  Sweden: "SE",
-  Switzerland: "CH",
-  Syria: "SY",
-  Taiwan: "TW",
-  Tajikistan: "TJ",
-  Tanzania: "TZ",
-  Thailand: "TH",
-  "Timor-Leste": "TL",
-  Togo: "TG",
-  Tonga: "TO",
-  "Trinidad and Tobago": "TT",
-  Tunisia: "TN",
-  Turkey: "TR",
-  Turkmenistan: "TM",
-  Tuvalu: "TV",
-  Uganda: "UG",
-  Ukraine: "UA",
-  "United Arab Emirates": "AE",
-  "United Kingdom": "GB",
-  "United States": "US",
-  Uruguay: "UY",
-  Uzbekistan: "UZ",
-  Vanuatu: "VU",
-  "Vatican City": "VA",
-  Venezuela: "VE",
-  Vietnam: "VN",
-  Yemen: "YE",
-  Zambia: "ZM",
-  Zimbabwe: "ZW",
-};
+function jsonToCountryList(jsonString: string): CountryDistribution {
+  let countryList = JSON.parse(jsonString);
+  // If ends with "?", it is a potential distribution
+  let known = countryList.filter((country: string) => !country.endsWith("?"));
+
+  let potential = countryList
+    .filter((country: string) => country.endsWith("?"))
+    .map((country: string) => country.slice(0, -1));
+
+  return { known, potential };
+}
+
+export type { CountryDistribution };
+export { splitCountryDistribution, countryListToJson, jsonToCountryList };
