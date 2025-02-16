dcea3b5042592f088b973843f75cb6918fb6caa1
Heru Handika <herubiolog@gmail.com>
2025-02-15 22:37:08 -0600

    Fix test.

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "yarn run dev",
-  //   url: "http://localhost:4321/",
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "yarn run dev",
+    url: "http://localhost:4321/",
+    reuseExistingServer: !process.env.CI,
+  },
 });
