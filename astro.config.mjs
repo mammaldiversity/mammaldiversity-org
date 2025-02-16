7bb62803bd9be96a3730ed5fca69b4816d342391
Heru Handika <herubiolog@gmail.com>
2025-02-16 17:45:55 -0600

    Switch back to beta address.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.mammaldiversity.org/",
-  // site: "https://www.mdd.hhandika.com/",
+  // site: "https://www.mammaldiversity.org/",
+  site: "https://www.mdd.hhandika.com/",
   integrations: [tailwind(), pagefind()],
 });
