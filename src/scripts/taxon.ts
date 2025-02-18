21a3da8e455cc7b7d378d197ff8014e48cea0e2a
Heru Handika <herubiolog@gmail.com>
2025-02-17 21:41:09 -0600

    Move permanent link to a function.

--- src/scripts/taxon.ts
+++ src/scripts/taxon.ts
@@ -1,15 +1,25 @@
-// Check if the taxon data contains "|" and replace it with  middle dot "·"
+// We change the "|" to " · " because it is more readable.
+// The middle dot is used extensively in many websites,
+// e.g., Youtube, Wikipedia, and Google.
 function cleanTaxonData(taxonData: string): string {
   return taxonData.includes("|") ? taxonData.split("|").join(" · ") : taxonData;
 }
 
-function createSynonymName(rootName: string, species: string, originalNameCombination?: string): string {
+function createSynonymName(
+  rootName: string,
+  species: string,
+  originalNameCombination?: string
+): string {
   return originalNameCombination
-  ? originalNameCombination
-  : `${species} ${rootName}`
+    ? originalNameCombination
+    : `${species} ${rootName}`;
 }
 
-function createAuthorityCitation(authority: string, year: number, withParentheses: boolean): string {
+function createAuthorityCitation(
+  authority: string,
+  year: number,
+  withParentheses: boolean
+): string {
   let citation = `${authority}, ${year}`;
   return withParentheses ? `(${citation})` : citation;
 }
