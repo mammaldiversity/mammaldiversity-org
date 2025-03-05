b16e2f4dbfbd20b6f7fda1a7648e183c59a37884
Heru Handika <herubiolog@gmail.com>
2025-03-04 20:04:53 -0600

    Add google tags.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mammaldiversity.org/",
   // site: "https://www.mdd.hhandika.com/",
-  integrations: [tailwind(), pagefind()],
+  integrations: [
+    tailwind(),
+    pagefind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
