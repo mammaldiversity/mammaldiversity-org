4f3285ff8a29c74d35a6ebfc28092815d9d026de
Heru Handika <herubiolog@gmail.com>
2025-06-11 10:09:59 -0400

    Improve 404 page design.

--- src/scripts/taxon.ts
+++ src/scripts/taxon.ts
@@ -1,8 +1,19 @@
 // We change the "|" to " · " because it is more readable.
 // The middle dot is used extensively in many websites,
 // e.g., Youtube, Wikipedia, and Google.
+// Some text contains commas, "," without the trailing space
+// like a typical sentence. This cause issue with word breaks.
+// We will add space after the comma if it is not present.
 function cleanTaxonData(taxonData: string): string {
-  return taxonData.includes("|") ? taxonData.split("|").join(" · ") : taxonData;
+  if (taxonData.includes("|")) {
+    return taxonData.split("|").join(" · ");
+  } else if (taxonData.includes(",")) {
+    return taxonData
+      .split(",")
+      .map((part) => part.trim())
+      .join(", ");
+  }
+  return taxonData;
 }
 
 function createSynonymName(
