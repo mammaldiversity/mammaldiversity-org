748c43f18c768823358c61902ecb65d6e534f1c0
Heru Handika <herubiolog@gmail.com>
2026-03-07 16:31:56 -0500

    Add mil parsers.

---
+++ db/mil_model.ts
@@ -0,0 +1,11 @@
+interface MilMetadata {
+    milId: number;
+    mddId: number;
+    description: string;
+    location: string;
+    photographer: string;
+    dateTaken: string;
+    filePath: string;
+}
+
+export type { MilMetadata }
