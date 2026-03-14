ba44598e0e0536d4c283bd9ab08a1fb41f91dbce
Heru Handika <herubiolog@gmail.com>
2026-03-14 16:02:50 -0400

    Fix json parsing and data imports.

--- db/mil.ts
+++ db/mil.ts
@@ -1,14 +1,11 @@
 // Parser for parsing the MIL json file
-import { readFileSync } from "fs";
 import type { MilMetadata } from "./mil_model";
+import milRaw from "../db/data/mil.json";
 
-const MIL_PATH = "./db/data/mil.json";
 const META_PARENT_DIR = "/src/assets/mil-images";
 
 function parseMilJson(): MilMetadata[] {
-    const rawData = readFileSync(MIL_PATH, "utf8");
-    const jsonData = JSON.parse(rawData);
-    return jsonData.map((item: any) => ({
+    return (milRaw as any[]).map((item) => ({
         milId: item.milId,
         mddId: item.mddId,
         description: item.description,
@@ -20,8 +17,7 @@ function parseMilJson(): MilMetadata[] {
 }
 
 function getMilMetadataByMddId(mddId: number): MilMetadata[] {
-    const allMilMetadata = parseMilJson();
-    return allMilMetadata.filter((mil) => mil.mddId === mddId);
+    return parseMilJson().filter((mil) => mil.mddId === mddId);
 }
 
 export { getMilMetadataByMddId, parseMilJson };
