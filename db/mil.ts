8831f4ec53669e260d604ab0fa8d5cd3e9a6c40a
Heru Handika <herubiolog@gmail.com>
2026-04-23 12:58:11 -0400

    Allow to filter images by orientation.

--- db/mil.ts
+++ db/mil.ts
@@ -5,22 +5,38 @@ import milRaw from "./data/mil.json";
 const META_PARENT_DIR = "/mil-images";
 
 function parseMilJson(): MilMetadata[] {
-    return (milRaw as any[]).map((item) => ({
-        milId: item.milId,
-        mddId: Number.parseInt(item.mddId, 10),
-        isUncertainIdentification: item.isUncertainIdentification ?? false,
-        description: item.description?.replace(/\|/g, ", ") ?? "",
-        location: item.location ?? "",
-        distribution: item.distribution ?? "",
-        photographer: item.photographer ?? "",
-        dateTaken: item.dateTaken ?? "",
-        filePath: `${META_PARENT_DIR}/${item.milId}.webp`,
-    }));
+  return (milRaw as any[]).map((item) => ({
+    milId: item.milId,
+    mddId: Number.parseInt(item.mddId, 10),
+    isUncertainIdentification: item.isUncertainIdentification ?? false,
+    description: item.description?.replace(/\|/g, ", ") ?? "",
+    location: item.location ?? "",
+    distribution: item.distribution ?? "",
+    orientation: item.orientation ?? "",
+    photographer: item.photographer ?? "",
+    dateTaken: item.dateTaken ?? "",
+    filePath: `${META_PARENT_DIR}/${item.milId}.webp`,
+  }));
 }
 
 function getMilMetadataByMddId(mddId: number): MilMetadata[] {
-    const allMilImages = parseMilJson();
-    return allMilImages.filter((mil) => mil.mddId === mddId);
+  const allMilImages = parseMilJson();
+  return allMilImages.filter((mil) => mil.mddId === mddId);
 }
 
-export { getMilMetadataByMddId, parseMilJson };
+function getLandscapeImages(): MilMetadata[] {
+  const allMilImages = parseMilJson();
+  return allMilImages.filter((mil) => mil.orientation === "landscape");
+}
+
+function getPortraitImages(): MilMetadata[] {
+  const allMilImages = parseMilJson();
+  return allMilImages.filter((mil) => mil.orientation === "portrait");
+}
+
+export {
+  getMilMetadataByMddId,
+  parseMilJson,
+  getLandscapeImages,
+  getPortraitImages,
+};
